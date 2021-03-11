33fbc7ef6edaf47c60de94183be8e71ffa5c925d
Majra <majra.oruc@gmail.com>
2021-03-11 23:08:51 +0100

    ex04 decimal and exponential notation

---
+++ ex04/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+    var myDecimal = 5.625;
+    var myDecimalE = 0.32e-3;
+    //Only change code below this line
+
+    //Only change code above this line
+    return {
+        myDecimal,
+        myDecimalE
+    };
+}
+
+console.log(main());
+module.exports = main;
