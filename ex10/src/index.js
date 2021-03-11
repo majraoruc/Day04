9e1d321d63370ca3de30cacee27720f1b89a3282
Majra <majra.oruc@gmail.com>
2021-03-11 23:43:31 +0100

    Declaring string variables

---
+++ ex10/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+    //Only change code below this line
+    var myFirstName = 'Majra';
+    var myLastName = 'Oruc';
+    var myHomeTown = 'Kiseljak';
+    //Only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    }
+}
+console.log(main());
+module.exports = main;
